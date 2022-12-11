bccf5cd986141bf29966d1dd79d264c081584f6a
MuhammedFurkanKaya <frknkybm@gmail.com>
2022-12-11 16:08:34 +0300

    Initial Commit

---
+++ BayrakUygulamasi/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  BayrakUygulamasi
+//
+//  Created by Muhammed Furkan Kaya on 11.12.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
